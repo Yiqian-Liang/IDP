c8a0988794d586f7cc6247d88dd41946b5042e23
Joseph Chanot <jc2477@cam.ac.uk>
2025-02-03 22:08:32 +0000

    Created a function called navigate which takes a list of actions which make up the route. This can be added to main if it works.

--- navigation.py
+++ navigation.py
@@ -1,90 +1,145 @@
+#for testing the Navigation function
 from motor import Wheel, Actuator  # Import the Wheel and Actuator classes
+from time import sleep 
+#from code_reader import QRCodeReader
 from line_sensor import LineSensor
-from distance_sensor import DistanceSensor
-from threading import Timer #To create timer interrupts
+#from distance_sensor import DistanceSensor
 from machine import Pin, PWM, I2C
-distance_sensor=DistanceSensor()
-code_reader=QRCodeReader()
-wheels = Wheel((7,6), (4, 5))
+#distance_sensor=DistanceSensor()
+#code_reader=QRCodeReader()
+wheels = Wheel((4,5),(7,6)) # Initialize the wheels (GP4, GP5 for left wheel, GP7, GP6 for right wheel) before the order was wrong
 actuator = Actuator(8, 9) # Initialize linear actuator (GP8 for direction, GP9 for PWM control)
 #navigation function
 # message_string=QRCodeReader.read_qr_code()
 # destination=QRCodeReader.parse_qr_data(message_string)
 sensors=[LineSensor(12),LineSensor(13),LineSensor(14),LineSensor(15)]
-
-def junction_detected(pin):
-    global junction_flag
-    junction_flag = 1  # Set the flag when an interrupt occurs
-
-def attach_interrupts():
-    sensors[0].pin.irq(trigger=Pin.IRQ_RISING, handler=junction_detected)
-    sensors[-1].pin.irq(trigger=Pin.IRQ_RISING, handler=junction_detected)
-
-def navigate(route):
-    '''
-    -Navigate takes an array of steps which make up the desired route.
-    -Each step consists of an array as follows [safety check, turning action, continuous action]
-    -If no turning action (e.g. want straight on) or no safety check (if doesn't work) use None
-    -The safety action should include a desired configuration of the 2 junction sensors
-    (e.g. [1,0] for left turn only, [1,1] for either...)
-    -The turn action should be turn left, turn right or 180 degree spin
-    -The continuous action should be one of 'pick up block', 'straight line follow' or 'place block'
-    -The first action presumes that there is no safety check or turn action required,
-    and will skip to the continuous action.
-    '''
-
-    n_steps = len(route)
-    cur_step = 0
-    global junction_flag 
-    junction_flag = 0
-
-    #Assign interrupts that set the flag to be 1 if either sensor detects a line
-    attach_interrupts()
-
-    #First action, only do the continuous action
-    while junction_flag == 0:
-        route[0][2]()
-
-    while True:
-        #When junction flag == 1
-        if junction_flag == 1:
-            cur_step += 1
-            #Perform safety check on junction if necessary
-            if route[cur_step][0] is not None:
-                while safety_check(route[cur_step][0]) == 0:
-                    #if failed check, maybe reverse?
-                    pass
-            
-            #Carry out turning if necessary
-            elif route[cur_step][1] is not None :
-
-                route[cur_step][1]()
-
-                #Temporarily unnattach interrupts
-                sensors[0].pin.irq(trigger = Pin.IRQ_RISING, handler = None)
-                sensors[-1].pin.irq(trigger = Pin.IRQ_RISING, handler = None)
-                junction_flag = 0
-
-                #Set timer to reattach interrupts once moved away from junction
-                #Junction recovery time may need adjusting
-                Timer(0.5, attach_interrupts)
-                
-        
-        else:
-            while junction_flag != 1:
-                #Perform continuous action until junction detected
-                route[cur_step][2]()
-                #The final action in the route should then call the next route
-                #This will depend on previous location, number of deliveries etc
-
-
-
-
-def safety_check(junction):
-    #simple check if the junction matches what we expect
-    if (sensors[0] == junction [0]) and (sensors[-1] == junction[-1]):
-        return 1
+#go to depot ->use distance sensor to detect distance->scan QR code->pick up->navigate to destination->drop off->go back to depot  (during this have some number n to record the number of boxes loaded)->depo2
+def sensor_status():
+    status=[]
+    for i in range(4):
+        status.append(sensors[i].read())
+        #print(f"Sensor {i+1}: {sensors[i].read()}")
+        sleep(0.1)
+    return status
+def line_following(status,direction=0):#line following function
+    """Follow the line using the line sensors"""
+    #print("Following the line...")
+    #Output: TTL(Black for LOW output, White for HIGH output)
+    #this is line following so junctions not included
+    #status=sensor_status()
+    if direction==1:
+        if status[0] == 0 and status[-1] == 0:
+            if status[1] == 1 :
+                wheels.turn_right(direction=1)
+            elif status[2] == 1 :
+                wheels.turn_left(direction=1)
+            else:
+                wheels.reverse()
     else:
-        return 0
+        if status[0] == 0 and status[-1] == 0:
+            if status[1] == 1 :
+                wheels.turn_left()  # All sensors are on the line, move forward
+            elif status[2] == 1 :
+                wheels.turn_right()
+            else:
+                wheels.forward()
 
-    
+def rotate_left():
+    # status=sensor_status()
+    # # Detect a junction (both left and right sensors detect the line)
+    # if status[0] == 1 or status[-1] == 1:
+        #print("Junction detected, turning...")
+        wheels.stop()  # Stop before turning
+        sleep(2)  # Short delay for stability
+        wheels.rotate_left(60)
+        sleep(3.2) #rotate long enough first to make sure the car deviate enough
+        #start_time = time.time()  # Start timing turn
+
+        while True:
+            wheels.rotate_left(40)  # Rotate left
+            status = sensor_status()  # Check sensor again
+            if status[2] == 1:  # If back on track, stop turning
+                wheels.stop()
+                sleep(0.5)
+                break
+
+def rotate_right():
+    # status=sensor_status()
+    # if status[0] == 1 or status[-1] == 1:
+        #print("Junction detected, turning...")
+        wheels.stop()  # Stop before turning
+        sleep(2)  # Short delay for stability
+        wheels.rotate_right(60)
+        sleep(3.2) #rotate long enough first to make sure the car deviate enough
+        #start_time = time.time()  # Start timing turn
+        while True:
+            wheels.rotate_right(40)  # Rotate right
+            status = sensor_status()  # Check sensor again
+            if status[1] == 1:  # If back on track, stop turning
+                wheels.stop()
+                sleep(0.5)
+                break
+def navigate_to(destination,pickup_location="Depot 1",):
+    """Navigate AGV based on pickup location."""
+    n = 0  # Track the number of times status[-1] is triggered
+    if pickup_location == "Depot 1":  # Right side
+        if destination == "A":
+            while True:  # Loop until reaching the destination
+                status = sensor_status()  # Read current sensor status
+                line_following(status,1)  # Execute line-following, in the reverse direction first
+                if status[-1] == 1:
+                    if n in [0]: # Execute left rotation only at the 1st occurrence of status[0] == 1, use in list so more convenient to adjust and add if necessary
+                        rotate_right()
+                        sleep(1)  # Adjust timing if necessary
+                        n += 1  # Update turn count
+                    else:
+                        n+=1
+                        pass
+                elif status[0] == 1:
+                    return destination  # Arrived at A's doorstep
+        elif destination == "B":
+            while True:
+                status = sensor_status()
+                line_following(status,1)
+                if status[-1] == 1:
+                    if n in [1]:
+                        rotate_right()
+                        n+=1
+                    else:
+                        n+=1
+                        pass
+                elif status[0] == 1:
+                    rotate_left()
+                    return destination
+        elif destination == "C":
+            while True:
+                status = sensor_status()
+                line_following(status,1)
+                if status[-1] == 1:
+                    if n in [1,2]:
+                        rotate_right()
+                        n+=1
+                    else:
+                        n+=1
+                        pass
+                elif status[0] == 1:
+                    rotate_left()
+                    return destination
+        elif destination == "D":
+            while True:
+                status = sensor_status()
+                line_following(status,1)
+                if status[-1] == 1:
+                    if n in [2]:
+                        rotate_right()
+                        n+=1
+                    else:
+                        n+=1
+                        pass
+                elif status[0] == 1:
+                    rotate_left()
+                    return destination
+def main():
+    navigate_to("A")
+if "name" == "__main__":
+    main()
