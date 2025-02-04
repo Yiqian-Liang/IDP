f2d49e9bbb95e4e1b5277079a67572a9db5ca953
Biggestmahe <157534365+Biggestmahe@users.noreply.github.com>
2025-02-04 10:41:24 +0000

    flashing light class

---
+++ flashing_light.py
@@ -0,0 +1,12 @@
+from machine import Pin
+
+class LED:
+    def __init__(self, pin=17):
+        self.pin = Pin(pin, Pin.OUT)
+
+    def start_flash (self):
+        self.pin.value(1)
+    def stop_flash (self) :
+        self.pin.value(0)
+        
+
